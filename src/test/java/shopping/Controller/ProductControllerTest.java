7520772ba1787e1f89df0c711146e8cd314382c3
harry4455 <xoxolittlemonster4455@gmail.com>
2024-08-19 15:56:07 +0900

    fix: 상품 수정하고 http로 통신하는 것 확인

--- src/test/java/shopping/Controller/ProductControllerTest.java
+++ src/test/java/shopping/Controller/ProductControllerTest.java
@@ -6,17 +6,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
 import shopping.Model.Product;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerTest {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private ProductController productController;
 
+    private RestClient restClient;
 
+    @BeforeEach
+    public void setUp() {
+        restClient = RestClient.create();
+    }
 
     @Test
     void 상품명으로_조회한다() {
@@ -36,7 +47,18 @@ public class ProductControllerTest {
     @Test
     void 상품을_수정한다() {
         Product product = new Product("url33333", "grape", 100000.0);
-        Product updatedProduct = productController.updateProduct(product.getName(), product);
-        assertThat(updatedProduct).isEqualTo(productController.getProducts(product.getName()));
+//        Product updatedProduct = productController.updateProduct(product.getName(), product);
+//        assertThat(updatedProduct).isEqualTo(productController.getProducts(product.getName()));
+
+        var url = "http://localhost:"+port+"/api/products?name={name}";
+        final Product actual = restClient.put()
+                .uri(url, product.getName())
+                .body(product)
+//                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(Product.class);
+
+        assertThat(actual).isEqualTo(product);
+        assertThat(actual.getPrice()).isEqualTo(100000.0);
     }
 }
